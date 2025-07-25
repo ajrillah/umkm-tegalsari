656986441c2efcf8a1394db72a456213355e934b
ajrillah <fakhri.ajril@gmail.com>
2025-07-25 16:31:21 +0700

    ADD PAGE:: UMKM Madu

--- src/pages/service/service-2.js
+++ src/pages/service/service-2.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import Header from '../../components/Layout/Header';
-import Footer from '../../components/Layout/Footer';
-import SiteBreadcrumb from '../../components/Common/Breadcumb';
-import bannerbg from '../../assets/images/background/page-title.jpg';
-import ServiceTwoMain from '../../components/ServiceOne/ServiceTwoMain';
-
+import React from "react";
+import Header from "../../components/Layout/Header";
+import Footer from "../../components/Layout/Footer";
+import SiteBreadcrumb from "../../components/Common/Breadcumb";
+import bannerbg from "../../assets/images/banner/madu1.jpeg";
+import ServiceTwoMain from "../../components/ServiceOne/ServiceTwoMain";
 
 const ServiceTwo = () => {
-    return (
-        <React.Fragment>
-            <Header
-                parentMenu='page'
-                secondParentMenu='services'
-                activeMenu='/service-2'
-            />
-            {/* breadcrumb-area-start */}
-            <SiteBreadcrumb
-                pageTitle="Our Service 2"
-                pageName="Our Service 2"
-                breadcrumbsImg={bannerbg}
-            />
-            {/* breadcrumb-area-start */}
-            {/* ServiceOneMain */}
-            <ServiceTwoMain />
-            {/* ServiceOneMain */}
-            
-            
-            <Footer />
-        </React.Fragment>
-    );
-}
+  return (
+    <React.Fragment>
+      <Header
+        parentMenu="page"
+        secondParentMenu="services"
+        activeMenu="/service-2"
+      />
+      {/* breadcrumb-area-start */}
+      <SiteBreadcrumb
+        pageTitle="Madu"
+        pageName="Our Service 2"
+        breadcrumbsImg={bannerbg}
+      />
+      {/* breadcrumb-area-start */}
+      {/* ServiceOneMain */}
+      <ServiceTwoMain />
+      {/* ServiceOneMain */}
 
+      <Footer />
+    </React.Fragment>
+  );
+};
 
 export default ServiceTwo;
